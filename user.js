f0e123ba4e34c8978d7be41581609ff0b588e183
puzzle1610 <puzzle1610@tpg.com.au>
2016-06-24 20:04:16 +1000

    Add files via upload

---
+++ user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         AutoTrimps
+// @namespace    https://github.com/zininzinin/AutoTrimps
+// @version      2.1a
+// @description  Automate all the trimps!
+// @author       zininzinin, spindrjr, Ishkaru
+// @include        *trimps.github.io*
+// @include        *kongregate.com/games/GreenSatellite/trimps
+// @grant        none
+// ==/UserScript==
+javascript:with(document)(head.appendChild(createElement('script')).src='https://raw.githubusercontent.com/puzzle1610/testingstuff/master/AutoTrimps2.js')._;
