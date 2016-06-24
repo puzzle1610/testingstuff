f32e8e93d6c2a3c9b6e5f92e51dc83280755ddd1
puzzle1610 <puzzle1610@tpg.com.au>
2016-06-24 21:12:56 +1000

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoTrimps
 // @namespace    https://github.com/zininzinin/AutoTrimps
-// @version      2.1a
+// @version      2.2
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru
 // @include        *trimps.github.io*
