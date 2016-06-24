9139bc852d323ee371d03dc2d7d734505b83b015
puzzle1610 <puzzle1610@tpg.com.au>
2016-06-24 20:59:59 +1000

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -847,8 +847,8 @@ function getBreedTime(remaining) {
 function initializeAutoTrimps() {
     debug('initializeAutoTrimps');
     loadPageVariables();
-    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://raw.githubusercontent.com/puzzle1610/testingstuff/master/AutoTrimps2.js')._;
-    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://zininzinin.github.io/AutoTrimps/Graphs.js')._;
+    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://puzzle1610.github.io/testingstuff/AutoTrimps2.js')._;
+    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://puzzle1610.github.io/testingstuff/Graphs.js')._;
     //javascript: with(document)(head.appendChild(createElement('script')).src = 'https://rawgit.com/zininzinin/AutoTrimps/spin/NewUI.js')._;
     //javascript: with(document)(head.appendChild(createElement('script')).src = 'https://rawgit.com/zininzinin/AutoTrimps/spin/Graphs.js')._;
     //why you no update, github aids
